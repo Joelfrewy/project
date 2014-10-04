d32e58d79e46af5ca02860d5de7c45875c412d13
Joelfrewy <Joelfrewin@gmail.com>
2014-10-04 15:47:25 +0800

    Completed Part G
    light reflects by fragments rather than vertices
    much more detailed

--- project1-start2014/project1/LINUX_OR_MINGW/fStart.glsl
+++ project1-start2014/project1/LINUX_OR_MINGW/fStart.glsl
@@ -1,28 +1,28 @@
 #version 150
 
-in  vec2 texCoord;  // The third coordinate is always 0.0 and is discarded
-in vec3 pos;
 in vec3 N;
+in vec3 pos;
+
+in  vec2 texCoord;  // The third coordinate is always 0.0 and is discarded
 out vec4 fColor;
 uniform vec3 AmbientProduct, DiffuseProduct, SpecularProduct;
-uniform vec4 LightPosition;
-uniform float Shininess;
 
+uniform float Shininess;
+uniform vec4 LightPosition;
 uniform sampler2D texture;
 
 void
 main()
 {
 	vec4 color;
-	// The vector to the light from the vertex    
-    vec3 Lvec = LightPosition.xyz - pos;
-	//float distance = length(Lvec);
+
+	vec3 Lvec = LightPosition.xyz - pos;
+	float distance = length(Lvec);
 	
-    // Unit direction vectors for Blinn-Phong shading calculation
-    vec3 L = normalize( Lvec );   // Direction to the light source
+	vec3 L = normalize( Lvec );   // Direction to the light source
     vec3 E = normalize( -pos );   // Direction to the eye/camera
     vec3 H = normalize( L + E );  // Halfway vector
-
+	
     // Compute terms in the illumination equation
     vec3 ambient = AmbientProduct;
 
@@ -31,14 +31,15 @@ main()
 
     float Ks = pow( max(dot(N, H), 0.0), Shininess );
     vec3  specular = Ks * SpecularProduct;
-    
+	if(H == vec3(0.0,0.0,0.0)){
+    specular = vec3(100.0,20.0,0.0);}
     if( dot(L, N) < 0.0 ) {
 	specular = vec3(0.0, 0.0, 0.0);
     } 
 
     // globalAmbient is independent of distance from the light source
     vec3 globalAmbient = vec3(0.1, 0.1, 0.1);
-    color.rgb = (globalAmbient  + ambient + diffuse + specular);///distance;
+    color.rgb = (globalAmbient  + ambient + diffuse + specular)/distance;
     color.a = 1.0;
 	
     fColor = color * texture2D( texture, texCoord * 2.0 );
--- project1-start2014/project1/LINUX_OR_MINGW/vStart.glsl
+++ project1-start2014/project1/LINUX_OR_MINGW/vStart.glsl
@@ -5,20 +5,19 @@ in  vec3 vNormal;
 in  vec2 vTexCoord;
 
 out vec2 texCoord;
-out vec3 pos;
 out vec3 N;
-
+out vec3 pos;
 uniform mat4 ModelView;
 uniform mat4 Projection;
 
 void main()
 {
-    // Transform vertex position into eye coordinates
-    vec3 pos = (ModelView * vPosition).xyz;
-
-	
+	pos = (ModelView * vPosition).xyz;
 	// Transform vertex normal into eye coordinates (assumes scaling is uniform across dimensions)
-    vec3 N = normalize( (ModelView*vec4(vNormal, 0.0)).xyz );
+    N = normalize( (ModelView*vec4(vNormal, 0.0)).xyz );
+	
+	// Unit direction vectors for Blinn-Phong shading calculation
+    
 	
     gl_Position = Projection * ModelView * vPosition;
     texCoord = vTexCoord;
