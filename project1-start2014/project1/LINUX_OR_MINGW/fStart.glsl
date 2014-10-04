90e9c998a1915d33328ae4de46935afc097ac762
Joelfrewy <Joelfrewin@gmail.com>
2014-10-04 14:06:34 +0800

    Began Part G
    however only ambient light shines through

--- project1-start2014/project1/LINUX_OR_MINGW/fStart.glsl
+++ project1-start2014/project1/LINUX_OR_MINGW/fStart.glsl
@@ -1,14 +1,45 @@
 #version 150
 
-in  vec4 color;
 in  vec2 texCoord;  // The third coordinate is always 0.0 and is discarded
-
+in vec3 pos;
+in vec3 N;
 out vec4 fColor;
+uniform vec3 AmbientProduct, DiffuseProduct, SpecularProduct;
+uniform vec4 LightPosition;
+uniform float Shininess;
 
 uniform sampler2D texture;
 
 void
 main()
 {
+	vec4 color;
+	// The vector to the light from the vertex    
+    vec3 Lvec = LightPosition.xyz - pos;
+	//float distance = length(Lvec);
+	
+    // Unit direction vectors for Blinn-Phong shading calculation
+    vec3 L = normalize( Lvec );   // Direction to the light source
+    vec3 E = normalize( -pos );   // Direction to the eye/camera
+    vec3 H = normalize( L + E );  // Halfway vector
+
+    // Compute terms in the illumination equation
+    vec3 ambient = AmbientProduct;
+
+    float Kd = max( dot(L, N), 0.0 );
+    vec3  diffuse = Kd*DiffuseProduct;
+
+    float Ks = pow( max(dot(N, H), 0.0), Shininess );
+    vec3  specular = Ks * SpecularProduct;
+    
+    if( dot(L, N) < 0.0 ) {
+	specular = vec3(0.0, 0.0, 0.0);
+    } 
+
+    // globalAmbient is independent of distance from the light source
+    vec3 globalAmbient = vec3(0.1, 0.1, 0.1);
+    color.rgb = (globalAmbient  + ambient + diffuse + specular);///distance;
+    color.a = 1.0;
+	
     fColor = color * texture2D( texture, texCoord * 2.0 );
 }
